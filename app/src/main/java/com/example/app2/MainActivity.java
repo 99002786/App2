23a99acea8ad0928f5db91ac206e277154fcee9d
Sharan megha <sharanmegha@gmail.com>
2020-11-12 10:00:48 +0530

    Shared preferences

--- app/src/main/java/com/example/app2/MainActivity.java
+++ app/src/main/java/com/example/app2/MainActivity.java
@@ -3,20 +3,28 @@ package com.example.app2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    public static final String MYPREFS = "myprefs";
+    public static final String NAMEKEY = "namekey";
+    public static final String PWDKEY = "pwdkey";
+    EditText nameEditText,pwdEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(TAG, "oncreate");
+        nameEditText =  findViewById(R.id.editTextTextPersonName);
+        pwdEditText = findViewById(R.id.editTextPwd);
     }
     @Override
     protected void onStart() {
@@ -28,14 +36,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         Log.i(TAG,"onpause");
+        saveData();
+    }
 
+    private void saveData() {
+        Log.i(TAG,"saveData");
+        //get the data from the edittext
+        String name = nameEditText.getText().toString();
+        String pwd = pwdEditText.getText().toString();
+        //create a file names myprefs
+        SharedPreferences preferences = getSharedPreferences(MYPREFS,MODE_PRIVATE);
+        //open the file
+        SharedPreferences.Editor editor = preferences.edit();
+        //write to the file
+        editor.putString(NAMEKEY,name);
+        editor.putString(PWDKEY,pwd);
+        //save the file
+        editor.apply();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Log.i(TAG,"onresume");
+        restoreData();
 
+    }
+
+    private void restoreData() {
+        Log.i(TAG,"restoreData");
+
+        //open the file
+        SharedPreferences preferences = getSharedPreferences(MYPREFS,MODE_PRIVATE);
+        //read the file
+        String name = preferences.getString(NAMEKEY,"");
+        String pwd = preferences.getString(PWDKEY,"");
+        //set the data in edittexts
+        nameEditText.setText(name);
+        pwdEditText.setText(pwd);
     }
 
     @Override
--- app/src/main/java/com/example/app2/MusicService.java
+++ app/src/main/java/com/example/app2/MusicService.java
@@ -2,6 +2,7 @@ package com.example.app2;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 import android.widget.Toast;
 
@@ -14,7 +15,12 @@ public class MusicService extends Service {
         super.onCreate();
         Toast.makeText(this, "service created", Toast.LENGTH_SHORT).show();
     }
-
+    public int onStartCommand(Intent intent, int flags, int startId){
+        super.onStartCommand(intent,flags,startId);
+        MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.music);
+        mediaPlayer.start();
+        return START_STICKY;
+    }
     @Override
     public void onDestroy() {
         super.onDestroy();
